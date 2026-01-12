8be38471594d989bf5687e2b45c75b53fc7d4b3b
Oscar Guerrero <oscard.guerrerom@outlook.com>
2026-01-12 16:40:39 -0500

    Adjusted sections: Projects, skills and footer to responsive

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -17,43 +17,45 @@ export default function Projects (){
 
 return(
 
-    <section id="projects" className="relative flex flex-col w-full h-lvh bg-cover bg-no-repeat bg-center py-2" style={{backgroundImage: `url(${current.images[0].image_url})`}}>
+    <section id="projects" className="relative flex flex-col md:flex-row justify-around items-center w-full h-auto md:h-lvh bg-cover bg-no-repeat bg-center py-2" style={{backgroundImage: `url(${current.images[0].image_url})`}}>
         <div className="absolute inset-0 bg-black/50"></div>
-        {/*Backgound*/}
-        <div className="relative flex flex-col justify-center items-center w-[50%] h-full">
-            <div className="absolute flex flex-col justify-baseline items-baseline top-40 left-10">
-                <h2 className="flex flex-row justify-baseline items-center font-bold text-white text-2xl font-poppins pb-2">{current.name}</h2>
-                <span className="w-[95%] h-0.5 rounded-full bg-jasmine"></span>
+            {/*Backgound*/}
+        <div className="order-2 md:order-1 relative flex flex-col justify-between items-center md:items-baseline w-full md:w-[50%] h-auto p-10 md:mt-0 mt-10">
+            <div className="title flex flex-col justify-baseline items-center md:items-baseline w-fit max-w-full">
+                <h2 className="font-bold text-white text-center md:text-left text-3xl font-poppins pb-2">{current.name}</h2>
+                <span className="w-full h-0.5 rounded-full bg-jasmine"></span>
             </div>
-            <div className="absolute left-10 bottom-45 flex flex-col justify-baseline align-middle w-120 h-auto">
-                <p className="text-white font-poppins">{current.desc}</p>
+            <div className="flex flex-col md:justify-baseline justify-center items-baseline w-[90%] h-auto p-4 mt-5 mb-15 md:mb-0">
+                <p className="text-white md:text-1xl lg:tex-3xl text-center md:text-left font-poppins">{current.desc}</p>
             </div>
-            <div className="absolute bottom-20 left-10 flex flex-row justify-around w-auto p-3 gap-5">
-            {
-                    current.tech.map((tech) => (
-                        <div className="flex flex-col text-white font-poppins bg-charcoalblue/90 p-2 border rounded-lg">
-                            <img src={tech.svg} alt={tech.techName} loading="lazy"/>
-                        </div>
-                    ))
-                }
+            <div className="flex flex-row justify-around items-center w-[90%] h-auto">
+                    <div className="flex flex-row justify-around w-auto p-3 gap-5">
+                        {
+                            current.tech.map((tech) => (
+                                <div className="flex flex-col text-white font-poppins bg-charcoalblue/90 p-2 border rounded-lg">
+                                    <img src={tech.svg} alt={tech.techName} loading="lazy"/>
+                                </div>
+                            ))
+                        }
+                    </div>
+                    <a className='group flex flex-row items-center gap-3 w-auto h-[70%] pl-6 pr-4 pt-2 pb-2 rounded-2xl bg-jasmine' href={`/projects/${current.slug}`}>
+                        <span className="w-auto h-auto text-sandibrown font-bold font-poppins">Explorar</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" 
+                        width="20" height="20" viewBox="0 0 24 24" 
+                        fill="none" stroke="#f4a261" stroke-width="2" 
+                        stroke-linecap="round" stroke-linejoin="round" 
+                        className="icon icon-tabler icons-tabler-outline icon-tabler-chevron-right transition-transform duration-300 group-hover:translate-x-2">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M9 6l6 6l-6 6" />
+                        </svg>
+                    </a>
             </div>
-            <a className='group absolute bottom-24 right-50 flex flex-row items-center gap-3 w-auto h-auto pl-6 pr-4 pt-2 pb-2 rounded-2xl bg-jasmine' href={`/projects/${current.slug}`}>
-                <span className="w-auto h-auto text-sandibrown font-bold font-poppins">Explorar</span>
-                <svg xmlns="http://www.w3.org/2000/svg" 
-                width="20" height="20" viewBox="0 0 24 24" 
-                fill="none" stroke="#f4a261" stroke-width="2" 
-                stroke-linecap="round" stroke-linejoin="round" 
-                className="icon icon-tabler icons-tabler-outline icon-tabler-chevron-right transition-transform duration-300 group-hover:translate-x-2">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                <path d="M9 6l6 6l-6 6" />
-                </svg>
-            </a>
         </div>
         {/* Carousel */}
-        <div className="absolute bottom-30 right-5 flex flex-row justify-center items-center w-150 h-70">
-            <div className="relative flex flex-row justify-baseline w-2xl overflow-hidden rounded-2xl">
-            {/* Slides */}
-                <div className="flex w-80 h-90 gap-5 transition-transform duration-500" 
+        <div className="order-1 md:order-2 relative flex flex-col justify-center items-center w-full md:w-150 h-auto gap-6 md:mt-0 mt-15">
+            <div className="relative flex flex-row justify-baseline  w-80 md:w-2xl overflow-hidden rounded-2xl">
+                {/* Slides */}
+                <div className="flex w-80 h-90 gap-6 transition-transform duration-500 pr-5" 
                 style={{ transform: `translateX(-${index * 100}%)` }}>
                     {projects.map((slide: any, i: any) => (
                     <div key={i} className="relative flex flex-col w-full shrink-0">
@@ -73,7 +75,7 @@ return(
                 </div>
             </div>
             {/* Controls */}
-            <div className="absolute -bottom-22 flex flex-row justify-between w-70 h-10">
+            <div className="flex flex-row justify-between w-50 md:w-70 h-10">
                 <div onClick={next} className="flex flex-row justify-center items-center w-10 h-10 rounded-full border-2 border-sandibrown hover:bg-jasmine/70">
                     <svg xmlns="http://www.w3.org/2000/svg" 
                     width="24" height="24" viewBox="0 0 24 24" 
