e5f58a37dfc65f973616510f2ae9b0e86ed84571
Oscar Guerrero <oscard.guerrerom@outlook.com>
2026-01-12 19:12:53 -0500

    Update file types for optimized images

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -7,11 +7,11 @@ export const projects: Project[] = [
         type: 'Branding',
         name: 'Café Abuela Elina',
         images: [
-            {id: 1, image_name: 'main_image', image_url: '/elina_vaso.jpg'},
-            {id: 2, image_name: 'second_imag', image_url: '/elina_taza.png'},
-            {id: 3, image_name: 'thirth_image', image_url: '/bolsa_negra.png'},
-            {id: 4, image_name: 'fourth_image', image_url: '/mockup_bolsa3.jpg'},
-            {id: 5, image_name: 'fifth_image', image_url: '/elina_vaso.jpg'}
+            {id: 1, image_name: 'main_image', image_url: '/elina_vaso.webp'},
+            {id: 2, image_name: 'second_imag', image_url: '/elina_taza.webp'},
+            {id: 3, image_name: 'thirth_image', image_url: '/bolsa_negra.webp'},
+            {id: 4, image_name: 'fourth_image', image_url: '/mockup_bolsa3.webp'},
+            {id: 5, image_name: 'fifth_image', image_url: '/elina_vaso.webp'}
         ],
         alt:'Abuela Elina',
         desc: 'Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.',
@@ -27,9 +27,9 @@ export const projects: Project[] = [
         type: 'Illustration',
         name: 'Renovación de imágen - Alpina',
         images: [
-            {id: 1, image_name: 'main_image', image_url: '/illustration.jpg'},
-            {id: 2, image_name: 'second_image', image_url: '/pallete.jpg'},
-            {id: 3, image_name: 'thirth_image', image_url: '/hero_bg.jpg'},
+            {id: 1, image_name: 'main_image', image_url: '/illustration.webp'},
+            {id: 2, image_name: 'second_image', image_url: '/pallete.webp'},
+            {id: 3, image_name: 'thirth_image', image_url: '/hero_bg.webp'},
         ],
         alt: 'Alpina',
         desc: 'Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.',
@@ -45,9 +45,9 @@ export const projects: Project[] = [
         type: 'Photography',
         name: 'Paola Ocampo - Spa de Uñas',
         images: [
-            {id: 1, image_name:'main_image' , image_url: '/photography.jpg' },
-            {id: 2, image_name:'second_image', image_url:'/branding.jpg'},
-            {id: 3, image_name:'thirth_image', image_url:'/drawing.jpg'}
+            {id: 1, image_name:'main_image' , image_url: '/photography.webp' },
+            {id: 2, image_name:'second_image', image_url:'/branding.webp'},
+            {id: 3, image_name:'thirth_image', image_url:'/drawing.webp'}
         ],
         alt: 'Paola Spa',
         desc: 'Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.',
--- src/lib/skills.ts
+++ src/lib/skills.ts
@@ -5,7 +5,7 @@ export const skills: Skills[] = [
     id:1,
     name: 'Ilustración Digital',
     level: 3,
-    img_url: '/illustration.jpg',
+    img_url: '/illustration.webp',
     desc: 'Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.',
     svg: '/icons/illustration.svg'
 },
@@ -13,7 +13,7 @@ export const skills: Skills[] = [
     id: 2,
     name: 'Fotografía',
     level: 3,
-    img_url: '/photography.jpg',
+    img_url: '/photography.webp',
     desc: 'Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.',
     svg: '/icons/photo.svg'
 },
@@ -21,7 +21,7 @@ export const skills: Skills[] = [
     id: 3,
     name: 'Dibujo Análogo',
     level: 5,
-    img_url: '/drawing.jpg',
+    img_url: '/drawing.webp',
     desc: 'Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.',
     svg: '/icons/drawing.svg'
 },
@@ -29,7 +29,7 @@ export const skills: Skills[] = [
     id: 4,
     name: 'Branding',
     level: 3,
-    img_url: '/branding.jpg',
+    img_url: '/branding.webp',
     desc: 'Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.',
     svg: '/icons/branding.svg'
 }
